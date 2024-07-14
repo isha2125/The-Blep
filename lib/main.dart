ce79fca62e0be6036f120d5c00a98c3cc57b05d1
isha2125 <2025isha@gmail.com>
2024-07-14 22:55:41 +0530

    Firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myntra_hackathon_prototype/firebase_options.dart';
 import 'screens/home_screen.dart';
 import 'screens/moving_character.dart';
 import 'screens/new_page.dart';
@@ -6,7 +8,11 @@ import 'screens/profile_page.dart';
 import 'screens/stores_page.dart';
 import 'screens/trendnxt_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
